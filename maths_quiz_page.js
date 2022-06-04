37c28d735b5fec101a40679c2e807a5c11f18023
shyamgarg <92663424+shyamgarg@users.noreply.github.com>
2022-06-04 10:02:19 +0530

    Add files via upload

--- maths_quiz_page.js
+++ maths_quiz_page.js
@@ -18,7 +18,7 @@ function send() {
     input_box = "<br>Answer : <input type='text' id='input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
     document.getElementById("output").innerHTML = row;
-   var row = question_number + input_box + check_button;
+   row = question_number + input_box + check_button;
     document.getElementById("number1").value = "";
     document.getElementById("number2").value = "";
 
